1020b7c286a9822e627e91df0a23463a6a40b530
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-06-23 17:48:44 +0300

    Create Install_Packages.js

---
+++ Install_Packages.js
@@ -0,0 +1,30 @@
+
+Incase you are encountering erro when installing a package, Just follow this procedure
+
+
+  conda install -c jupyter jupyter
+
+
+    Just repeadt the name twice
+
+      e.g
+        conda install -c pytorch pytorch
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+..
