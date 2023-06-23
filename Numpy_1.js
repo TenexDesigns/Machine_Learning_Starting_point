152bc60a9ed336841df1fb7c6abd342e48dba5da
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-06-23 20:22:25 +0300

    Create Numpy_1.js

---
+++ Numpy_1.js
@@ -0,0 +1,95 @@
+import numpy 
+
+my_list1 = [10,20,30,40,50,60,70]
+my_list =numpy.array(my_list1)
+
+print("Array:",my_list)
+print("Size:",my_list.size)
+print("Data Type:",my_list.dtype)
+print("Dimension:",my_list.ndim)
+print("Shape:",my_list.shape)
+
+
+
+Result
+*************
+
+Array: [10 20 30 40 50 60 70]
+Size: 7
+Data Type: int32
+Dimension: 1
+Shape: (7,)
+
+
+
+
+
+
+
+
+
+
+2. Matrix
+
+import numpy 
+
+my_list1 = [[10,20],
+            [30,40],
+            [50,60],
+            [70,80]]
+my_list =numpy.array(my_list1)
+
+print("Matrix:",my_list)
+print("Size:",my_list.size)
+print("Data Type:",my_list.dtype)
+print("Dimension:",my_list.ndim)
+print("Shape:",my_list.shape)
+
+
+  Result
+  *****
+
+ Matrix: [[10 20]
+ [30 40]
+ [50 60]
+ [70 80]]
+Size: 8
+Data Type: int32
+Dimension: 2
+Shape: (4, 2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+....
