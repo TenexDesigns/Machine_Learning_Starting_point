0d201726bd53a251bb1af3ccd194387287add458
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-06-23 10:36:42 +0300

    Create Why Neural Networks can learn (almost) anything.js

---
+++ Why Neural Networks can learn (almost) anything.js
@@ -0,0 +1,70 @@
+To understanfd How a neural Netwotk can Lear anything at all
+
+
+  We nned Stor Start at FUNCTIONS
+
+    What is a function
+
+
+
+      Funtion is a system of inputs and outputes, Numbers in And Numbers outs
+
+
+              x -----> f(x)   ----->
+
+
+
+numbers in    -----> Function -----> Numbers out
+
+                In the aboeve Case , you input X and the function outputs Y
+
+                  You can plot all the x and y values on a graph to give you a line
+                    What is important to know is that , as long as you know the function , you can always calculate thecorrect  output y, for any input x
+
+                      But say , we dont kow the function, but we only know some of its x and y values, we know the inputs and outputs ,but we don t know the function used to produce them
+                        Is there a way to reverse engineer that function that produce this data,
+                          If we could contrusct such a function, we could use it to calculate a y value given an x value that is not within our original dataset
+                          To construct this function, we need a function aprrocimation, called Funtion Aproximator, That is what a neural network is   --> f(x) = T(x)
+
+                  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+...
