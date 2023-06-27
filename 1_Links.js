6222323f551fec6f90359bae585fe8c94e977570
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-06-27 19:51:22 +0300

    Create 1_Links.js

---
+++ 1_Links.js
@@ -0,0 +1,10 @@
+
+Machine Learning Applications
+
+https://www.projectpro.io/article/10-awesome-machine-learning-applications-of-today/364#mcetoc_1gimosg6d32
+
+
+Machine Learning Framworks 
+
+https://www.projectpro.io/article/machine-learning-frameworks/509
+
